abf6988670b9b1f625e7890b82e3b581838ff6c6
Reda Al-Bahrani <5760087+reda-bahrani@users.noreply.github.com>
2022-04-08 11:21:17 -0500

    updates to use new tweepy version

--- code/tweepy_example.py
+++ code/tweepy_example.py
@@ -1,17 +1,23 @@
 from codes import consumer_key, consumer_secret, access_token, access_token_secret
-from tweepy.streaming import StreamListener
+# from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import socket
+import json
 
-class StdOutListener(StreamListener):
+class TweetListener(Stream):
     """ A listener handles tweets that are received from the stream.
     This is a basic listener that just prints received tweets to stdout.
-
     """
+    def __init__(self, *args, csocket):
+        super().__init__(*args)
+        self.client_socket = csocket
+
     def on_data(self, data):
-        print(data)
+        msg = str(json.loads(data))
+        print(msg)
         with open("tweets.json", "a") as f:
-            f.write(data)
+            f.write(msg)
         return True
 
 
@@ -20,10 +26,10 @@ class StdOutListener(StreamListener):
 
 
 if __name__ == '__main__':
-    l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    c_socket = socket.socket()
+    listener = TweetListener(
+        consumer_key, consumer_secret,
+        access_token, access_token_secret, 
+        csocket=c_socket)
 
-
-    stream = Stream(auth, l)
-    stream.filter(track=['covid-19', 'trump economy'])
+    listener.filter(track=['covid-19', 'the great resignation'])
